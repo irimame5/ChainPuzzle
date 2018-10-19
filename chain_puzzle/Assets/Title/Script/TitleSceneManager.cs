a057a91959857289417cc6e5bfdeee93385e5ec9
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2018-10-19 17:54:36 +0900

    タイトル追加

---
+++ chain_puzzle/Assets/Title/Script/TitleSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    [SerializeField,SceneName]
+    string nextScene;
+	
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
